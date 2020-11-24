d05ed0777b248ee4f6e253f5ad3f0c295433894d
Tim Mace <47009653+Maced2020@users.noreply.github.com>
2020-11-23 16:10:17 -0800

    Update zip_and_gunzip_linux.py

--- zip_and_gunzip_linux.py
+++ zip_and_gunzip_linux.py
@@ -1,8 +1,13 @@
 #! /usr/bin/python3
-# I am atempting to automaticly zip and unzip large files
+# I am atempting to automaticly zip and unzip large files (with no user input)
+# so far i have gotten to the point where i ask the users 
 
 import os
 
+
+# for linunx only
+# This function asks the user if they would like to zip or unzip files
+# This function also takes in files from the user and zips them for the user 
 def zip_and_gunzip():
     choice = ''
     while choice is not 'ZIP' or choice is not 'UNZIP':
@@ -10,9 +15,6 @@ def zip_and_gunzip():
             break
         choice = input('would you like to Zip or Unzip a file? ').upper()
          
-        
-
-        
         if choice == 'ZIP':
             multiple_files = []
             file_name = input('Give me a File(s) to Zip:')
