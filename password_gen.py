03e5a3e9ce48fb4c78da1566ae681159171000bf
Tim Mace <47009653+Maced2020@users.noreply.github.com>
2023-10-22 07:32:03 -0700

    Update password_gen.py
    Updated the password generator so that i can have multiple passwords generated.

--- password_gen.py
+++ password_gen.py
@@ -1,22 +1,55 @@
-#this will be a password genterator
-#started 1/22/2022
-# This now generates passwords as of 1/23/2022
-#
-import random
+# This is a password generator
 
-# this function 
+# completed 10/7/2023
+
+
+import random
+import sys
+
+
+
+
+
+# this function generates a random password: 
+
 def password_generator():
+
     alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=`~;'?./"
     userchoice = input("How long would you like your password to be?: ")
     password = " "
     try:
         for i in range(int(userchoice)):
             randomletter = random.choice(alphabet)
+            
             password = randomletter + password
         print(password)
+        print()
+
     except:
         print("You are clearly not following the rules; you must be a hacker")
+        sys.exit()
+
+    print()
 
 
-#this runs the functions and hands out a password
-password_generator()
+
+
+
+
+
+# this function allows the password generator to run as many times as the user would like
+
+def multiple_passwords():
+    while True:
+        again = input("would you like A password?: ")
+        print()
+        if again.upper() == "YES" or again.upper() == "Y":
+            password_generator()
+        else:
+            print()
+            print("No more passwords will be generated")
+            sys.exit()
+
+
+
+multiple_passwords()
