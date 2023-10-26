87c993cdab1390e6b6a6755bbabca156ee67ef7d
Tim Mace <47009653+Maced2020@users.noreply.github.com>
2023-10-25 18:55:10 -0700

    Add files via upload
    this will be An app that will help me generate a grocery list

---
+++ Grocery_list_generator.py
@@ -0,0 +1,91 @@
+# the plan is to turn this into a grocery list generator
+
+
+groceries = (
+"Jimmy John dupe",                    #0
+"French bread",                       #1
+"Deli Turkey",                        #2
+"provalone cheese",                   #3
+"mustard",                            #4
+"mayo",                               #5
+"tomatoes",                           #6
+"lettuce",                            #7 
+"Cheesesteaks",                       #8
+"onions",                             #9
+"French bread",                       #10
+"shaved beef",                        #11
+"milk",                               #12
+"chedder cheese",                     #13
+"provalone cheese",                   #14
+"spaghetti w/ meat sauce",            #15
+"angel hair pasta",                   #16
+"sauce",                              #17
+"Ground beef",                        #18
+"Garlic bread ",                      #19
+"Salad",                              #20
+"Thai chili chicken wraps",           #21
+"Costco Nuggies",                     #22
+"tortilla shells",                    #23
+"thai chili sauce",                   #24
+"lettuce",                            #25
+"chedder cheese",                     #26
+"Breakfast burritos",                 #27
+"eggs",                               #28
+"bacon",                              #29
+"sausage",                            #30
+"tortilla shells",                    #31
+"chedder cheese",                     #32
+"hashbrowns",                         #33
+"hot sauce",                          #34
+"Tacos/Natchos",                      #35
+"onions",                             #36
+"ground beef",                        #37
+"taco shells",                        #38
+"refried beans",                      #39
+"cheese",                             #40
+"lettuce",                            #41
+"tomatos",                            #42
+"sour cream",                         #43
+"guacamole",                          #44
+"hot sauce",                          #45
+"Enchiladas",                         #46
+"rotisserie chicken",                 #47
+"ground beef",                        #48
+"Enchilada sauce",                    #49
+"tortilla shells",                    #50
+"chedder cheese",                     #51
+"corn",                               #52
+"green chillis",                      #53
+"sour cream",                         #54
+"Pan chicken",                        #55
+"chicken breast",                     #56
+"Onions",                             #57
+"potatoes",                           #58
+"cream cheese",                       #59
+"veggie of choice",                   #60
+"Chicken sausage alfrado",            #61
+"Chicken sausage",                    #62
+"angel hair pasta",                   #63
+"cream cheese",                       #64
+"garlic",                             #65
+"Sloppy joes",                        #66
+"hamburger buns",                     #67
+"Sloppy Joe manwich sauce",           #68
+"Ground beef",                        #69
+"chedder cheese",                     #70
+"Fried rice",                         #71
+"teriyaki chicken meal",              #72
+"teriyaki sauce",                     #73
+"Chicken strips",                     #74
+"chicken breast",                     #75
+"fryer Oil",                          #76
+"Chicken breading",                   #77
+"beer",                               #78
+"onions",                             #79
+) 
+
+
+tacos = groceries[35:45]
+
+for i in tacos:
+    print (i)
